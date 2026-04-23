6da9c932428f238a1b22f8d2a9e0cdc883a80b6d
krxzy-k <kabelopheeha55@gmail.com>
2026-04-23 19:06:16 +0200

    adde the save function

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let count = 0
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-El")
 
 function increment() {
-    count = count + 1
+    count += 1
     countEl.innerText = count
 }
 
 function save() {
-    console.log(count)
+    let entries = count + " - "
+    saveEl.textContent += entries
+    count = 0
+    countEl.textContent = count
 }
